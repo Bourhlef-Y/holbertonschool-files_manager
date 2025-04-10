51b20f71a0887c37180dff146852df89918239ad
J.Laborne <laborne.j@hotmail.fr>
2025-04-10 11:52:55 +0200

    T6

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -2,8 +2,8 @@ import { v4 as uuidv4 } from 'uuid';
 import { promises as fs } from 'fs';
 import path from 'path';
 import { ObjectId } from 'mongodb';
-import redisClient from '../utils/redis.mjs';
-import dbClient from '../utils/db.mjs';
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
 
 const FOLDER_PATH = process.env.FOLDER_PATH || '/tmp/files_manager';
 
@@ -86,6 +86,37 @@ class FilesController {
       type,
       isPublic,
       parentId,
+    });
+  }
+
+  static async getShow(req, res) {
+    const token = req.headers['x-token'];
+    if (!token) return res.status(401).json({ error: 'Unauthorized' });
+
+    const userId = await redisClient.get(`auth_${token}`);
+    if (!userId) return res.status(401).json({ error: 'Unauthorized' });
+
+    let fileId;
+    try {
+      fileId = new ObjectId(req.params.id);
+    } catch (err) {
+      return res.status(404).json({ error: 'Not found' });
+    }
+
+    const file = await dbClient.db.collection('files').findOne({
+      _id: fileId,
+      userId: new ObjectId(userId),
+    });
+
+    if (!file) return res.status(404).json({ error: 'Not found' });
+
+    return res.status(200).json({
+      id: file._id.toString(),
+      userId: file.userId.toString(),
+      name: file.name,
+      type: file.type,
+      isPublic: file.isPublic,
+      parentId: file.parentId === 0 ? 0 : file.parentId.toString(),
     });
   }
 }
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
-import FilesController from '../controllers/FilesController.js';
+import FilesController from '../controllers/FilesController';
 
 const router = express.Router();
 
@@ -21,5 +21,9 @@ router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
 
 router.post('/files', FilesController.postUpload);
+
+router.get('/files/:id', FilesController.getShow);
+
+router.get('/files', FilesController.getIndex);
 
 export default router;
